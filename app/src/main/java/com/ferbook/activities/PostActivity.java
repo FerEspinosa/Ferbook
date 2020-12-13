5e364690304e441f471d80715681e0155a48f7a1
FerEspinosa <elmundodefer@gmail.com>
2020-12-12 22:01:37 -0300

    Busca imagen en galería y la muestra en la activity_post.xml

--- app/src/main/java/com/ferbook/activities/PostActivity.java
+++ app/src/main/java/com/ferbook/activities/PostActivity.java
@@ -1,16 +1,66 @@
 package com.ferbook.activities;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.ferbook.R;
+import com.ferbook.utils.FileUtil;
+
+import java.io.File;
 
 public class PostActivity extends AppCompatActivity {
+
+    ImageView mImgView_Post1;
+    private final int gallery_request_code = 1;
+    File mImageFile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
+
+        mImgView_Post1 = findViewById(R.id.iv_post1);
+        mImgView_Post1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openGallery();
+            }
+        });
+    }
+
+    private void openGallery() {
+        Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        //La siguiente línea configura que el Intent abre la galería del teléfono
+        galleryIntent.setType("image/*");
+        startActivityForResult(galleryIntent, gallery_request_code);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == gallery_request_code && resultCode == RESULT_OK) {
+            try {
+                //transformar la URI en el archivo mImageFile
+                mImageFile = FileUtil.from(this, data.getData());
+
+                // mostrar la imagen en el primer cardview (Post1)
+                mImgView_Post1.setImageBitmap(BitmapFactory.decodeFile(mImageFile.getAbsolutePath()));
+
+
+            } catch (Exception e){
+                Log.d ("ERROR", "se produjo un error"+e.getMessage());
+                Toast.makeText(this, "se produjo un error"+e.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        }
+
     }
 }
