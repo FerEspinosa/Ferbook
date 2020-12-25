ae1f4c78bd4444521633b7de741a8d5f9aca937e
FerEspinosa <elmundodefer@gmail.com>
2020-12-25 18:35:52 -0300

    Diseñada la activity_edit_profile.xml

--- app/src/main/java/com/ferbook/activities/EditProfileActivity.java
+++ app/src/main/java/com/ferbook/activities/EditProfileActivity.java
@@ -3,14 +3,27 @@ package com.ferbook.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.ferbook.R;
 
+import de.hdodenhof.circleimageview.CircleImageView;
+
 public class EditProfileActivity extends AppCompatActivity {
+
+    CircleImageView mBack_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
+
+        mBack_button = findViewById(R.id.btn_atras);
+        mBack_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
