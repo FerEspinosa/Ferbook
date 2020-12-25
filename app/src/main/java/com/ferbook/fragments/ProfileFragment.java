499827bd1726d590e125c0ac02dc99842fe84b0f
FerEspinosa <elmundodefer@gmail.com>
2020-12-25 17:52:46 -0300

    Agregado el botón Editar perfil. Creada la activity editProfile. El botón funciona ok

--- app/src/main/java/com/ferbook/fragments/ProfileFragment.java
+++ app/src/main/java/com/ferbook/fragments/ProfileFragment.java
@@ -1,5 +1,6 @@
 package com.ferbook.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,13 +8,18 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import com.ferbook.R;
+import com.ferbook.activities.EditProfileActivity;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ProfileFragment extends Fragment {
+
+    LinearLayout mLinearLayoutEditProfile;
+    View mView;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -24,6 +30,21 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        mView = inflater.inflate(R.layout.fragment_profile, container, false);
+        mLinearLayoutEditProfile = mView.findViewById(R.id.linearLayout_EditProfile);
+        mLinearLayoutEditProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToEditProfile();
+            }
+        });
+        return mView;
+
+    }
+
+    private void goToEditProfile() {
+        Intent intent = new Intent(getContext(), EditProfileActivity.class);
+        startActivity(intent);
+
     }
 }
