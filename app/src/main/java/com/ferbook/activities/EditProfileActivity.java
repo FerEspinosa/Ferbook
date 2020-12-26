7451a714c3fa50d556196e07fdcec80989d81847
FerEspinosa <elmundodefer@gmail.com>
2020-12-25 21:06:24 -0300

    Interfaz de activity_edit_profile.xml INSTANCIADA

--- app/src/main/java/com/ferbook/activities/EditProfileActivity.java
+++ app/src/main/java/com/ferbook/activities/EditProfileActivity.java
@@ -4,26 +4,63 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.ferbook.R;
+import com.google.android.material.textfield.TextInputEditText;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class EditProfileActivity extends AppCompatActivity {
 
-    CircleImageView mBack_button;
+    CircleImageView     mImageView_Back_button;
+    Button              mButton_update;
+
+    CircleImageView     mImageView_profile;
+    ImageView           mImageView_cover;
+    TextInputEditText   mInput_name;
+    TextInputEditText   mInput_phone;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
 
-        mBack_button = findViewById(R.id.btn_atras);
-        mBack_button.setOnClickListener(new View.OnClickListener() {
+        mInput_name             = findViewById(R.id.input_nombre);
+        mInput_phone            = findViewById(R.id.input_telefono);
+
+        // IMAGEN DE PERFIL
+        mImageView_profile      = findViewById(R.id.circleImage_Profile);
+        mImageView_profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // CAMBIAR FOTO DE PERFIL
+            }
+        });
+
+        // IMAGEN DE PORTADA
+        mImageView_cover        = findViewById(R.id.imageViewCover);
+
+        // BOTON ACTUALIZAR
+        mButton_update          = findViewById(R.id.btn_actualizar);
+        mButton_update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //ACTUALIZAR DATOS
+            }
+        });
+
+        // BOTON ATRAS
+        mImageView_Back_button  = findViewById(R.id.btn_atras);
+        mImageView_Back_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
             }
         });
+
     }
 }
