0f4b5489e3e4d8796660d737730abecf40b72efc
FerEspinosa <elmundodefer@gmail.com>
2020-12-29 01:13:54 -0300

    Muestra las imagenes de las publicaciones(post) en un slider (Clase 45 terminada) Funciona OK

--- app/src/main/java/com/ferbook/activities/PostDetailActivity.java
+++ app/src/main/java/com/ferbook/activities/PostDetailActivity.java
@@ -73,7 +73,6 @@ public class PostDetailActivity extends AppCompatActivity {
 
         //configurar animacion
 
-
         mSliderAdapter = new SliderAdapter(PostDetailActivity.this, mSliderItems);
         mSliderView.setSliderAdapter(mSliderAdapter);
 
