bb9d926c9069bbdbd25b5138bdd9904219d083bf
FerEspinosa <elmundodefer@gmail.com>
2021-03-15 16:48:35 -0300

    Diseñada la pantalla del chat y el toolbar personalizado Chat (clase 69 completada)

--- app/src/main/java/com/ferbook/activities/ChatActivity.java
+++ app/src/main/java/com/ferbook/activities/ChatActivity.java
@@ -1,8 +1,13 @@
 package com.ferbook.activities;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
 
 import com.ferbook.R;
 import com.ferbook.models.Chat;
@@ -17,16 +22,32 @@ public class ChatActivity extends AppCompatActivity {
 
     ChatProvider mChatProvider;
 
+    View mActionBarView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+        showCustomToolbar(R.layout.custom_chat_toolbar);
+
 
         mExtraUserId1 = getIntent().getStringExtra("userId1");
         mExtraUserId2 = getIntent().getStringExtra("userId2");
 
         mChatProvider = new ChatProvider();
         createChat();
+    }
+
+    private void showCustomToolbar(int resource) {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("");
+        actionBar.setDisplayShowHomeEnabled(true);
+        actionBar.setDisplayShowCustomEnabled(true);
+        LayoutInflater inflater = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        mActionBarView = inflater.inflate(resource, null);
+        actionBar.setCustomView(mActionBarView);
     }
 
     private void createChat () {
